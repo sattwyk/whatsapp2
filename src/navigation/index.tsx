8267f6219fa145c99926de93f497996332599c3d
Sattwik <hellosattwik@gmail.com>
2022-10-15 21:46:47 +0530

    added bottom navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import MainTabNavigator from './MainTabNavigator';
 import ChatsScreen from '../screens/ChatsScreen';
 import ChatScreen from '../screens/ChatScreen';
 
@@ -10,6 +10,11 @@ export default function Navigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen
+          name='Main'
+          component={MainTabNavigator}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name='Chats' component={ChatsScreen} />
         <Stack.Screen name='Chat' component={ChatScreen} />
       </Stack.Navigator>
